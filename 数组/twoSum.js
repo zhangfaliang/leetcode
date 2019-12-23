66f65ae77d3d96bc0e5b42c7a2a84d10032106e3
ShinyZhang <120965880@qq.com>
2019-12-23 23:30:47 +0800

    :construction: add  removeElement

--- 数组/twoSum.js
+++ 数组/twoSum.js
@@ -1,4 +1,4 @@
-/***
+/** *
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
@@ -66,7 +66,7 @@ var twoSum = function(nums, target) {
 利用使用while 结合x%10的余数，赋值给一个变量*10累加实现数组翻转，值得注意的一点是要判断不断让x/10 取整数
 
  */
-var isPalindrome = function(x) {
+const isPalindrome = function(x) {
   let s = 0;
   let x1 = x;
   while (x1 > 0) {
@@ -83,9 +83,9 @@ var isPalindrome = function(x) {
 
 示例 1:
 
-给定数组 nums = [1,1,2], 
+给定数组 nums = [1,1,2],
 
-函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。 
+函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
 
 你不需要考虑数组中超出新长度后面的元素。
 示例 2:
@@ -131,7 +131,7 @@ var removeDuplicates = function(nums) {
 };
 
 /**
- * 
+ *
  * @param {罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
 
 字符          数值
@@ -147,7 +147,7 @@ M             1000
 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
 
 I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
-X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。
 C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
 
@@ -198,14 +198,14 @@ var romanToInt = function(s) {
     if (i > 0) {
       const specialCurrent = `${s[i - 1]}${s[i]}`;
       if (specialCurrent in enumerationObj) {
-        res = res + enumerationObj[specialCurrent];
-        i = i - 1;
+        res += enumerationObj[specialCurrent];
+        i -= 1;
         continue;
       }
-      res = res + enumerationObj[current];
+      res += enumerationObj[current];
       continue;
     }
-    res = res + enumerationObj[current];
+    res += enumerationObj[current];
   }
 
   return res;
@@ -244,4 +244,64 @@ var romanToInt = function(s) {
   }
   res += pre;
   return res;
+};
+
+/**
+ * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+示例 1:
+
+给定 nums = [3,2,2,3], val = 3,
+
+函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+
+你不需要考虑数组中超出新长度后面的元素。
+示例 2:
+
+给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+
+函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+
+注意这五个元素可为任意顺序。
+
+你不需要考虑数组中超出新长度后面的元素。
+说明:
+
+为什么返回数值是整数，但输出的答案是数组呢?
+
+请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+
+你可以想象内部操作如下:
+
+// nums 是以“引用”方式传递的。也就是说，不对实参作任何拷贝
+int len = removeElement(nums, val);
+
+// 在函数里修改输入数组对于调用者是可见的。
+// 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+/*
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = function(nums, val) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[sum] = nums[i];
+      sum++;
+    }
+  }
+  return sum;
 };
