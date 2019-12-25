cd6c19d37f86c793749ff44a5acdb91cdc61050e
ShinyZhang <120965880@qq.com>
2019-12-25 14:40:39 +0800

    :construction: add merge

--- 数组/twoSum.js
+++ 数组/twoSum.js
@@ -424,3 +424,55 @@ const maxSubArray1 = function(nums) {
   }
   return max;
 };
+/**
+ *
+ * @param {给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+
+如果不存在最后一个单词，请返回 0 。
+
+说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+
+示例:
+
+输入: "Hello World"
+输出: 5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/length-of-last-word
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。}
+ */
+const lengthOfLastWord = function(s) {
+  const srtArr = s.replace(/(^ +| +$)/g, '').split(' ');
+  return srtArr[srtArr.length - 1].length;
+};
+
+/**
+ * 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+
+说明:
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+示例:
+
+输入:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+输出: [1,2,2,3,5,6]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+const merge = function(nums1, m, nums2, n) {
+  nums1.splice(m, nums1.length - m, ...nums2);
+  nums1.sort((a, b) => a - b);
+};
