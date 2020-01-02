485f0cb031de7e15469651c4f6e199ff3c325975
ShinyZhang <120965880@qq.com>
2020-01-02 17:58:28 +0800

    :sparkles: thirdMax

--- 数组/twoSum.js
+++ 数组/twoSum.js
@@ -1,4 +1,4 @@
-/***
+/** *
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
@@ -48,7 +48,7 @@ var twoSum = function(nums, target) {
 利用使用while 结合x%10的余数，赋值给一个变量*10累加实现数组翻转，值得注意的一点是要判断不断让x/10 取整数
 
  */
-var isPalindrome = function(x) {
+const isPalindrome = function(x) {
   let s = 0;
   let x1 = x;
   while (x1 > 0) {
@@ -57,3 +57,50 @@ var isPalindrome = function(x) {
   }
   return s == x;
 };
+/**
+ * 给定一个非空数组，返回此数组中第三大的数。如果不存在，则返回数组中最大的数。要求算法时间复杂度必须是O(n)。
+
+示例 1:
+
+输入: [3, 2, 1]
+
+输出: 1
+
+解释: 第三大的数是 1.
+示例 2:
+
+输入: [1, 2]
+
+输出: 2
+
+解释: 第三大的数不存在, 所以返回最大的数 2 .
+示例 3:
+
+输入: [2, 2, 3, 1]
+
+输出: 1
+
+解释: 注意，要求返回第三大的数，是指第三大且唯一出现的数。
+存在两个值为2的数，它们都排第二。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/third-maximum-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @param {*} nums
+ */
+const thirdMax = function(nums) {
+  let current = null;
+  let num = 0;
+  nums.sort((a, b) => b - a);
+  for (item of nums) {
+    if (current !== item) {
+      current = item;
+      num++;
+      if (num === 3) return item;
+    }
+  }
+  e;
+  if (num < 3) return nums[0];
+};
+
+console.log(thirdMax([1, 2]));
